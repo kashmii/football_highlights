50bbc4a197756bc88aca251941afffe963aa82a5
kashmii <www.myac@gmail.com>
2024-06-12 20:36:07 +0900

    メソッド細分化

--- app/services/jleague_games_service.rb
+++ app/services/jleague_games_service.rb
@@ -7,13 +7,23 @@ require 'nokogiri'
 class JleagueGamesService < ApplicationService
   # Service Objectが自らの責務を果たすために複数の手順が必要になった場合、手順をトランザクションでラップするとよい
   def call(game_week)
-    tmp_file = access_webpage
+    tmp_file = get_webpage_html
     collect_game_attributes(tmp_file)
   end
 
   private
     # 該当ページのHTMLをtmpファイルに保存
-    def access_webpage
+    def get_webpage_html
+      driver = setup_driver
+      html = fetch_html(driver)
+      doc = Nokogiri::HTML.parse(html)
+      file_path = save_html(doc)
+
+      driver.quit
+      file_path
+    end
+
+    def setup_driver
       # Selenium WebDriverの設定
       options = Selenium::WebDriver::Chrome::Options.new
       options.add_argument('--headless') # ヘッドレスモード（GUIなし）で実行する場合
@@ -22,77 +32,89 @@ class JleagueGamesService < ApplicationService
 
       # Selenium WebDriverの初期化
       driver = Selenium::WebDriver.for :chrome, options: options
+    end
 
+    def fetch_html(driver)
       # ブラウザを開いて指定したURLにアクセス
       driver.get(ENV["J1SHOW_URL"])
-
       # JavaScriptの実行を待つ
       sleep 10
-
-      # # ページのHTMLを取得
       html = driver.page_source
+    end
 
-      doc = Nokogiri::HTML.parse(html)
-
-      # 現在の日時を含むファイル名を生成
+    def save_html(doc)
       timestamp = Time.now.strftime("%Y%m%d%H%M%S")
       filename = "tmpfile_#{timestamp}.html"
       file_path = File.join(Dir.tmpdir, filename)
 
-      # HTMLを保存
       # TODO: tmpfileは削除されずに残るので、後で削除するようにする
       File.open(file_path, 'w') do |file|
         file.write(doc)
       end
 
-      driver.quit
       file_path
     end
 
+    # ===========================
+    # get_webpage_html 関連ここまで
+    # ===========================
+
     def collect_game_attributes(file)
-      # ===================================
-      # 変数定義など
       game_attrs = []
-      # TODO: 直に書いてしまっているので、後で別のところに書く
-      target_text = "第 18 日（全 38 日）"
+      target_text = "第 #{ENV["GAME_WEEK_NUM"]} 日（全 38 日）"
 
-      # ===================================
-
-      html = File.read(file)
-      doc = Nokogiri::HTML(html)
-
-      # 節テーブル(class OcbAbf)を取得している
-      target_element = doc.css(".GVj7ae:contains('#{target_text}')")[0].parent.parent
-      this_tbody = target_element.at('tbody')
-
+      doc = parse_html_from_file(file)
+      # 節テーブル(class OcbAbf)のtbodyを取得している
+      target_element = find_target_element(doc, target_text)
       # # <tr> = 2試合が入っている行 | <td> = 1試合が入っている列
       # # this_tbodyの子要素である<tr>要素の中の<td>要素すべてを選択します
-      td_arr = this_tbody.css('> tr > td')
+      td_arr = target_element.css('> tr > td')
 
       td_arr.each do |td|
-        # tr 3つ目 日時
-        third_tr = td&.at('tr:nth-child(3)')
-
-        date_div = third_tr&.at('.imspo_mt__date')
-        time_div = date_div&.next_element
-
-        date_str = date_div&.text
-        time_str = time_div&.text
-
-        # tr 5つ目 ホーム/ 6つ目 アウェイ
-        fifth_tr = td&.at('tr:nth-child(5)')
-        sixth_tr = td&.at('tr:nth-child(6)')
-
-        home = fifth_tr&.at('.liveresults-sports-immersive__hide-element')
-        away = sixth_tr&.at('.liveresults-sports-immersive__hide-element')
+        home, away, date_str, time_str = extract_game_info(td)
 
         if home && away && date_str && time_str
-          game_attrs << [ENV["GAME_WEEK_NUM"], parse_datetime(date_div&.text, time_div&.text), home.text, away.text]
+          game_attrs << [ENV["GAME_WEEK_NUM"], parse_datetime(date_str, time_str), home.text, away.text]
         end
       end
 
+      # 本番は puts 除去
       puts game_attrs
     end
+
+    def parse_html_from_file(file)
+      html = File.read(file)
+      Nokogiri::HTML(html)
+    end
+
+    def find_target_element(doc, target_text)
+      doc.css(".GVj7ae:contains('#{target_text}')")[0].parent.parent.at('tbody')
+    end
+
+    def extract_game_info(td)
+      # tr 3つ目 日時
+      third_tr = td&.at('tr:nth-child(3)')
+
+      date_div = third_tr&.at('.imspo_mt__date')
+      time_div = date_div&.next_element
+
+      date_str = date_div&.text
+      time_str = time_div&.text
+
+      # tr 5つ目 ホーム/ 6つ目 アウェイ
+      fifth_tr = td&.at('tr:nth-child(5)')
+      sixth_tr = td&.at('tr:nth-child(6)')
+
+      home = fifth_tr&.at('.liveresults-sports-immersive__hide-element')
+      away = sixth_tr&.at('.liveresults-sports-immersive__hide-element')
+
+      [home, away, date_div&.text, time_div&.text]
+    end
+
+    # ===========================
+    # collect_game_attributes 関連ここまで
+    # ===========================
+
 
     # 文字列の日付と時刻をDateTimeオブジェクトに変換
     def parse_datetime(date_part, time_part)
